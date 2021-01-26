35f0a9147386d0fb904a7d2c80de9d7828e66cc4
蔡金锋 <jeff.cai@99bill.com>
2021-01-26 12:58:25 +0800

    类型调整

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,40 +1,40 @@
-interface CardInfo {
-  bankName: string;
-  bankCode: string;
-  cardBin: string;
-  cardType: string;
-  cardTypeName: string;
-  length: number;
+declare namespace bankcard {
+  interface CardInfo {
+    bankName: string;
+    bankCode: string;
+    cardBin: string;
+    cardType: string;
+    cardTypeName: string;
+    length: number;
+  }
+
+  type Options = {
+    filter?: (data: CardInfo) => object;
+    format?: (data: CardInfo) => any;
+    maxCacheCount?: number;
+  }
+
+  type SearchReturn = CardInfo | CardInfo[] | null;
+
+  interface ValidateCardInfoReturn extends Partial<CardInfo> {
+    validated: boolean;
+    errorCode: string;
+    message: string;
+  }
+
+  export class CardBin {
+    constructor(options: Options);
+    searchCardBin(cardNo: string, multiple?: boolean): SearchReturn;
+    validateCardInfo(cardNo: string): ValidateCardInfoReturn;
+  }
+
+  export const bank: Record<string, string>;
+  export const cardType: Record<string, string>;
+  export const getAllCard: () => CardInfo[];
+
+  export default CardBin;
 }
 
-type Options = {
-  filter?: (data: CardInfo) => object;
-  format?: (data: CardInfo) => any;
-  maxCacheCount?: number;
-}
+export as namespace bankcard;
 
-type SearchReturn = CardInfo | CardInfo[] | null;
-
-interface ValidateCardInfoReturn extends Partial<CardInfo> {
-  validated: boolean;
-  errorCode: string;
-  message: string;
-}
-
-declare class CardBin {
-  constructor(options: Options);
-  searchCardBin(cardNo: string, multiple?: boolean): SearchReturn;
-  validateCardInfo(cardNo: string): ValidateCardInfoReturn;
-}
-
-type bank = Record<string, string>;
-type cardType = Record<string, string>;
-type getAllCard = () => CardInfo[];
-
-export type {
-  bank,
-  cardType,
-  getAllCard
-}
-
-export default CardBin;
+export = bankcard;
